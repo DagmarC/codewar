2a342b105c2e9250fbf70302093493d544ae1e8e
DagmarMac <dg,cervenkova@gmail.com>
2023-11-29 14:14:22 +0100

    leetcode: poisoned duration

--- leetcode/arrays/teemo_attack.go
+++ leetcode/arrays/teemo_attack.go
@@ -3,11 +3,10 @@ package arrays
 
 func findPoisonedDuration(timeSeries []int, duration int) int {
     var total int
-
+	
 	if len(timeSeries) == 0 {
 		return total
 	}
-
 	for i := 0; i < len(timeSeries)-1; i++ {
 		if timeSeries[i+1] <= timeSeries[i]+duration {
 			total += timeSeries[i+1] - timeSeries[i]
@@ -15,7 +14,5 @@ func findPoisonedDuration(timeSeries []int, duration int) int {
 			total += duration
 		}
 	}
-	total += duration // i == len(timeSeries)-1  (last element)
-
-	return total
+	return total + duration
 }
