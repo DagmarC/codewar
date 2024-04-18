af36f5564a395049e53ab664a9dd7648ce27df29
DagmarMac <dg,cervenkova@gmail.com>
2024-04-18 09:29:07 +0200

    Leetcode 198 Huse robber problem

--- leetcode/dynamicprog/house_robber.go
+++ leetcode/dynamicprog/house_robber.go
@@ -1,1 +1,42 @@
 package dynamicproblems
+
+func rob(nums []int) int {
+	dp := make(map[int]int, len(nums))
+
+	var robRec func(int) int
+	robRec = func(i int) int {
+		if i < 0 {
+			return 0
+		}
+		if i == 0 || i == 1 {
+			return nums[i]
+		}
+		if value, ok := dp[i]; ok {
+			return value
+		}
+		dp[i] = max(dp[i], nums[i]+robRec(i-2), nums[i]+robRec(i-3))
+		return dp[i]
+	}
+	return max(robRec(len(nums)-1), robRec(len(nums)-2))
+}
+
+func robLinear(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = nums[1]
+
+	for i := 2; i < len(nums); i++ {
+		if i==2 {
+			dp[i] = nums[i] + dp[i-2]
+			continue
+		}
+		dp[i] = max(nums[i]+dp[i-2], nums[i]+dp[i-3])
+	}
+	return max(dp[len(nums)-1], dp[len(nums)-2])
+}
--- leetcode/dynamicprog/house_robber_test.go
+++ leetcode/dynamicprog/house_robber_test.go
@@ -16,3 +16,23 @@ package dynamicproblems
 // Output: 12
 // Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
 // Total amount you can rob = 2 + 9 + 1 = 12.
+
+import "testing"
+
+func TestRob(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  int
+	}{
+		{input: []int{1, 2, 3, 1}, want: 4},
+		{input: []int{2, 7, 9, 3, 1}, want: 12},
+		{input: []int{2, 1, 1, 2}, want: 4},
+		{input: []int{226, 174, 214, 16, 218, 48, 153, 131, 128, 17, 157, 142, 88, 43, 37, 157, 43, 221, 191, 68, 206, 23, 225, 82, 54, 118, 111, 46, 80, 49, 245, 63, 25, 194, 72, 80, 143, 55, 209, 18, 55, 122, 65, 66, 177, 101, 63, 201, 172, 130, 103, 225, 142, 46, 86, 185, 62, 138, 212, 192, 125, 77, 223, 188, 99, 228, 90, 25, 193, 211, 84, 239, 119, 234, 85, 83, 123, 120, 131, 203, 219, 10, 82, 35, 120, 180, 249, 106, 37, 169, 225, 54, 103, 55, 166, 124}, want: 7102},
+	}
+	for _, tc := range tests {
+		var got int
+		if got = robLinear(tc.input); got != tc.want {
+			t.Errorf("House Robber: got %d want %d", got, tc.want)
+		}
+	}
+}
